a155715256258a377f4f74f53b5b3f83423696cb
MelodicNoob <jryan8831@gmail.com>
2021-03-10 20:57:51 +0800

    refactor: Separated gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,72 @@
-const { dest, series, src, watch, parallel, task } = require('gulp');
-const sass = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
-const rename = require('gulp-rename');
-const babel = require('gulp-babel');
-const ulgify = require('gulp-uglify');
-const del = require('del');
+const { dest, series, src, watch, parallel, task } = require("gulp");
+const sass = require("gulp-sass");
+const cleanCSS = require("gulp-clean-css");
+const rename = require("gulp-rename");
+const babel = require("gulp-babel");
+const ulgify = require("gulp-uglify");
+const del = require("del");
 
-sass.compiler = require('node-sass');
+sass.compiler = require("node-sass");
 
-async function cleanAssetsDist(cb) {
-    await del(['assets/dist']);
-    cb();
+async function cleanAssetsDist() {
+    await del(["assets/dist"]);
 }
 
-async function cleanAssetsSrcSass(cb) {
-    await del(['assets/src/sass']);
-    cb();
+async function cleanAssetsSrcSass() {
+   await del(["assets/src/sass/**"]);
 }
 
-function copyBootstrapAssets(cb) {
-    src("node_modules/bootstrap/scss/**")
+function copyBootstrapAssets() {
+    return src("node_modules/bootstrap/scss/**")
     .pipe(dest("assets/src/sass/bootstrap"));
-    cb();
 }
 
-function copyFontAwesomeAssets(cb) {
-    src("node_modules/@fortawesome/fontawesome-free/scss/**")
+function copyFontAwesomeAssets() {
+    return src("node_modules/@fortawesome/fontawesome-free/scss/**")
     .pipe(dest("assets/src/sass/fontawesome"));
-    cb();
 }
 
-function js(cb) {
-    src('./assets/js/*')
+function js() {
+    return src("assets/js/*")
     .pipe(babel({
-        presets: ['@babel/env']
+        presets: ["@babel/env"]
     }))
-    .pipe(dest('./assets/dist/js'));
-    cb();
+    .pipe(dest("assets/dist/js"));
 }
 
-function minifyCSS(cb) {
-    src('./assets/css/*.css')
+function minifyCSS() {
+    return src("assets/css/*.css")
     .pipe(cleanCSS())
     .pipe(rename({
         extname: ".min.css"
     }))
-    .pipe(dest('assets/dist/css'));
-    cb();
+    .pipe(dest("assets/dist/css"));
 }
 
-function minifyJS(cb) {
-    src('./assets/dist/js/*.js')
+function minifyJS() {
+    return src("assets/dist/js/*.js")
     .pipe(ulgify())
     .pipe(rename({
         extname: ".min.js"
     }))
-    .pipe(dest('assets/dist/js'));
-    cb();
+    .pipe(dest("assets/dist/js"));
 }
 
-function scss(cb) {
-    src('./assets/sass/theme.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(dest('./assets/css'));
-    cb();
+function scss() {
+    return src("assets/sass/theme.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("assets/css"));
 }
 
 exports.build = series(
-    parallel(cleanAssetsSrcSass, cleanAssetsDist),
-    parallel(copyBootstrapAssets, copyFontAwesomeAssets),
-    parallel(scss, js), 
+    parallel(scss, js),
     parallel(minifyCSS, minifyJS)
 );
 
-exports.watch = series(
-    parallel(cleanAssetsSrcSass, cleanAssetsDist), 
-    parallel(copyBootstrapAssets, copyFontAwesomeAssets),
-    function() {
-        watch(['assets/sass/theme.scss', 'assets/js/*.js'], parallel(scss, js))
-    }
-);
+exports.clean = series(cleanAssetsSrcSass, cleanAssetsDist);
+
+exports.copyAssets = series(copyBootstrapAssets, copyFontAwesomeAssets);
+
+exports.watch = function() {
+    watch(["assets/sass/theme.scss", "assets/js/*.js"], parallel(scss, js))
+}
