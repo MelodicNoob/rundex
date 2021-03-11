b316d21d0b086f788beb1851cb40d616a07b5b01
MelodicNoob <jryan8831@gmail.com>
2021-03-11 12:28:36 +0800

    feat: add functionality to load theme's css and js

--- functions.php
+++ functions.php
@@ -14,3 +14,23 @@ require_once('routes.php');
 
 // Set global params in the Timber context
 add_filter('timber_context', [$lumberjack, 'addToContext']);
+
+add_action('wp_head', 'setup_rundex_css');
+add_action('wp_footer', 'setup_rundex_js');
+
+
+function setup_rundex_css() {
+    wp_register_style('rundex-css', 
+        get_template_directory_uri() . '/assets/dist/css/theme.min.css',
+        [], null, 'all'
+    );
+    wp_enqueue_style('rundex-css');
+}
+
+function setup_rundex_js() {
+    wp_register_script('rundex-js', 
+        get_template_directory_uri() . '/assets/dist/js/theme.min.js', 
+        [ 'jquery' ], null, true
+    );
+    wp_enqueue_script('rundex-js');
+}
