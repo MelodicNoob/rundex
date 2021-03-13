c2dad0cd99ceef2ec9559d6ad9569da49fa1f836
MelodicNoob <jryan8831@gmail.com>
2021-03-13 18:41:12 +0800

    feat: add code to fetch front page banner customizer data

--- front-page.php
+++ front-page.php
@@ -16,6 +16,9 @@ class FrontPageController extends Controller {
     public function handle() {
         $context = Timber::get_context();
 
+        $context['banner_heading'] = get_theme_mod( 'front_page_banner_heading' );
+        $context['banner_sub_heading'] = get_theme_mod( 'front_page_banner_sub_heading' );
+
         return new TimberResponse('templates/front-page.twig', $context);
     }
 }
